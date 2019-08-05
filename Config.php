feb8a6a787529bc55a940c639f14ae8d0a57ae4c
MrYuri123 <13524t@gmail.com>
2019-08-05 13:19:07 +0300

    update spaces

--- Config.php
+++ Config.php
@@ -2,12 +2,12 @@
 
 class Config
 {
-	//avaliable IP adresses.
-	const IP = ['127.0.0.1'];
+    //avaliable IP adresses.
+    const IP = ['127.0.0.1'];
 
-	//database config
+    //database config
     const DB_HOST = 'localhost';
-	const DB_NAME = 'testapi';
-	const DB_USER = 'root';
-	const DB_PASS = '';
+    const DB_NAME = 'testapi';
+    const DB_USER = 'root';
+    const DB_PASS = '';
 }
